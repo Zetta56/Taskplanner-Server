380c6dc15911835d5dfd80ac9d37903df32ab772
Zetta56 <67248032+Zetta56@users.noreply.github.com>
2020-09-12 18:31:21 -0400

    correct token schema name

--- models/Token.js
+++ models/Token.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const tokenSchema = new mongoose.Schema({
 	token: String,
 	userId: String,
 	createdAt: {type: Date, expires: 604800, default: Date.now} //Expires in 1 week
 });
 
-module.exports = mongoose.model("Token", userSchema);
+module.exports = mongoose.model("Token", tokenSchema);
